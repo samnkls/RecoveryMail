f6f020c24c2472a081b9294571db464790b5be52
samnkls <samuelnikolas500@gmail.com>
2025-01-14 02:41:42 -0300

    Update Footer.tsx

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center">
           <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/1280px-Gmail_icon_%282020%29.svg.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg"
             alt="Logo"
             width={150}
             height={60}
