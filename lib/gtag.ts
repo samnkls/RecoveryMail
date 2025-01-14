1fe8c98875e4102c0405963e31b15feaa7fcccd8
samnkls <samuelnikolas500@gmail.com>
2025-01-14 16:27:29 -0300

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,5 +1,5 @@
 // Google Ads Pixel Measurement ID
-export const GOOGLE_ADS_ID = "AW-16808569026";
+export const GOOGLE_ADS_ID = "AW-11549189858";
 
 // Carregar a tag do Google Ads Pixel
 export const loadGoogleAds = () => {
